2806bcdbf192ee3f1278d75e9c990bda5f846ad9
Gilmar Moraes <gilmar14moraes@hotmail.com>
2021-09-09 13:12:39 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/acenelio" target="_blank" rel="noreferrer">Nelio Alves</a> / 
+                <a href="https://github.com/Gilmar-moraes" target="_blank" rel="noreferrer"> Gilmar Moraes </a>
+                </p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
